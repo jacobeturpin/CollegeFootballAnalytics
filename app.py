264e04e70dc5a5001dada7832a1745ff169df143
jacobeturpin <jacobeturpin@gmail.com>
2016-06-08 21:26:12 -0400

    resolve weird issue where file was giving indentation error

--- app.py
+++ app.py
@@ -2,9 +2,21 @@
     for analysis
 """
 
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return "Hello World!"
+
+
 def main():
-	""" Main application function """
-    pass
+    """ Test """
+    
+    app.run('0.0.0.0')
+    return None
 
 
 if __name__ == '__main__':
