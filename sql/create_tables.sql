d36527d95a51d4e61e1d6dc843d6ef3e69709e01
jacobeturpin <jacobeturpin@gmail.com>
2016-06-10 11:00:57 -0400

    added game and box score tables to sql script

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -7,11 +7,63 @@ CREATE TABLE Team (
 CREATE TABLE Player (
 	PlayerId		VARCHAR(36)		NOT NULL,
 	PlayerName		VARCHAR(200)	NOT NULL,
-	PlayerLink		VARCAHR(250)	NOT NULL
+	PlayerLink		VARCHAR(250)	NOT NULL
 );
 
 CREATE TABLE Conference (
 	ConferenceId	VARCHAR(36)		NOT NULL,
 	ConferenceName	VARCHAR(200)	NOT NULL,
 	ConferenceLink	VARCHAR(250)	NOT NULL
+);
+
+CREATE TABLE Game (
+	GameId			VARCHAR(36)		NOT NULL,
+	GameLink		VARCHAR(250)	NOT NULL,
+	HomeTeamId		VARCHAR(36)		NOT NULL,
+	HomeTeamScore	INTEGER			NOT NULL,
+	AwayTeamId		VARCHAR(36)		NOT NULL,
+	AwayTeamScore	INTEGER			NOT NULL
+);
+
+CREATE TABLE PassingBoxScore (
+	GameId						VARCHAR(36)		NOT NULL,
+	PlayerId					VARCHAR(36)		NOT NULL,
+	TeamId						VARCHAR(36)		NOT NULL,
+	PassCompletions				INTEGER			NOT NULL,
+	PassAttempts				INTEGER			NOT NULL,
+	PassCompletionPercentage	DECIMAL			NOT NULL,
+	PassYards					INTEGER			NOT NULL,
+	PassYardsPerAttempt			DECIMAL			NOT NULL,
+	PassAdjustedYardsPerAttempt	DECIMAL			NOT NULL,
+	PassTouchdowns				INTEGER			NOT NULL,
+	PassInterceptions			INTEGER			NOT NULL,
+	PassEfficiencyRating		DECIMAL			NOT NULL
+);
+
+CREATE TABLE RushReceiveBoxScore (
+	GameId						VARCHAR(36)		NOT NULL,
+	PlayerId					VARCHAR(36)		NOT NULL,
+	TeamId						VARCHAR(36)		NOT NULL
+	-- Stats Go Here
+);
+
+CREATE TABLE DefenseBoxScore (
+	GameId						VARCHAR(36)		NOT NULL,
+	PlayerId					VARCHAR(36)		NOT NULL,
+	TeamId						VARCHAR(36)		NOT NULL
+	-- Stats Go Here
+);
+
+CREATE TABLE KickPuntReturnBoxScore (
+	GameId						VARCHAR(36)		NOT NULL,
+	PlayerId					VARCHAR(36)		NOT NULL,
+	TeamId						VARCHAR(36)		NOT NULL
+	-- Stats Go Here
+);
+
+CREATE TABLE KickPuntBoxScore (
+	GameId						VARCHAR(36)		NOT NULL,
+	PlayerId					VARCHAR(36)		NOT NULL,
+	TeamId						VARCHAR(36)		NOT NULL
+	-- Stats Go Here
 );
