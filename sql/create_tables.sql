01c2061262168634b27c0b7519b2a5b9739cd634
jacobeturpin <jacobeturpin@gmail.com>
2016-06-10 10:24:01 -0400

    Added Player and Conference tables to sql script

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,6 +1,17 @@
-CREATE TABLE TEAM (
+CREATE TABLE Team (
 	TeamId			VARCHAR(36)		NOT NULL,
 	TeamName		VARCHAR(200)	NOT NULL,
 	TeamLink		VARCHAR(250)	NOT NULL	
 );
 
+CREATE TABLE Player (
+	PlayerId		VARCHAR(36)		NOT NULL,
+	PlayerName		VARCHAR(200)	NOT NULL,
+	PlayerLink		VARCAHR(250)	NOT NULL
+);
+
+CREATE TABLE Conference (
+	ConferenceId	VARCHAR(36)		NOT NULL,
+	ConferenceName	VARCHAR(200)	NOT NULL,
+	ConferenceLink	VARCHAR(250)	NOT NULL
+);
