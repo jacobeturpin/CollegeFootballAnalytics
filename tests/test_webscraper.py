20d24b1a3a39e8c9dd2fd5825a7e8a531cf185a3
jacobeturpin <jacobeturpin@gmail.com>
2016-06-27 20:23:33 -0400

    added placeholder test methods for WebScraperTest

--- tests/test_webscraper.py
+++ tests/test_webscraper.py
@@ -5,9 +5,26 @@ class WebScraperTest(unittest.TestCase):
     """ Tests for web scraping components """
 
     def test_get_games_for_date(self):
+        """ Test links returned against predefined games for Oct 8, 2015 """
 
         expected_games = ['/cfb/boxscores/2015-10-08-houston.html',
                           '/cfb/boxscores/2015-10-08-southern-california.html']
-
         self.assertEqual(expected_games, get_all_games_for_date(2015, 10, 8))
 
+    def get_game_summary_info(self):
+        pass
+
+    def get_passing_stats(self):
+        pass
+
+    def get_rush_receive_stats(self):
+        pass
+
+    def get_defense_stats(self):
+        pass
+
+    def get_return_stats(self):
+        pass
+
+    def get_kick_punt_stats(content):
+        pass
