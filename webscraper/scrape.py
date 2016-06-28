ad9a8fde5bcc91b7937e548be517e724dcd589eb
jacobeturpin <jacobeturpin@gmail.com>
2016-06-27 21:11:18 -0400

    updated function to properly extract the desired statistical components from html elements

--- webscraper/scrape.py
+++ webscraper/scrape.py
@@ -10,18 +10,22 @@ import re
 __url_root = 'http://sports-reference.com/cfb/'
 
 
-def clean_html(item):
-   """ Takes html from scraping and removes unecessary elements """
-   return list(filter(lambda x: x != '\n', item))
+def filter_html_list(items):
+   """ Takes list of html from scraping and removes unecessary elements """
+
+   return list(filter(lambda x: x != '\n', items))
 
 def extract_components_from_html(list):
-    """ Updates list html elements into desired statistical components """
+    """ Updates list of html elements with desired statistical components """
 
     for idx, value in enumerate(list):
-        if value.contents.has_attr('a') in list:
-            list[idx] = ('name', 'link') # Placeholder for links
+        tag = value.find('a')
+
+        if tag:
+            list[idx] = (tag.string, tag['href'])
         else:
-            list[idx] = value.contents # Placeholder for numerical data
+           list[idx] = value.string if value.contents else 0
+
     return list
 
 def get_table_container(content, text):
@@ -71,35 +75,35 @@ def get_passing_stats(content):
 
     header = get_table_container(content, 'Passing')
     html = [x.parent.parent.contents for x in header.find_all('a', href=re.compile('.*player.*'))]
-    return list(map(clean_html, html))
+    return list(map(filter_html_list, html))
 
 def get_rush_receive_stats(content):
     """ Retrieves player-level rush and receiving data for specified game """
     
     header = get_table_container(content, 'Rushing & Receiving')
     html = [x.parent.parent.contents for x in header.find_all('a', href=re.compile('.*player.*'))]
-    return list(map(clean_html, html))
+    return list(map(filter_html_list, html))
 
 def get_defense_stats(content):
     """ Retrieves player-level defensive data for specified game """
     
     header = get_table_container(content, 'Defense & Fumbles')
     html = [x.parent.parent.contents for x in header.find_all('a', href=re.compile('.*player.*'))]
-    return list(map(clean_html, html))
+    return list(map(filter_html_list, html))
 
 def get_return_stats(content):
     """ Retrieves player-level punt/kick return data for specified game """
     
     header = get_table_container(content, 'Kick & Punt Returns')
     html = [x.parent.parent.contents for x in header.find_all('a', href=re.compile('.*player.*'))]
-    return list(map(clean_html, html))
+    return list(map(filter_html_list, html))
 
 def get_kick_punt_stats(content):
     """ Retrieve player-level kicking data for specified game """
     
     header = get_table_container(content, 'Kicking & Punting')
     html = [x.parent.parent.contents for x in header.find_all('a', href=re.compile('.*player.*'))]
-    return list(map(clean_html, html))
+    return list(map(filter_html_list, html))
 
 def execute_game_data_collection(link):
     """ Function used to collect game data for specified link """
