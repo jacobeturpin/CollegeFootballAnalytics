0998e7eb2dd343d33a456f69369956609027f080
Jacob Turpin <jacobeturpin@gmail.com>
2016-07-09 19:15:20 -0400

    finished remaining web scraping tests

--- tests/test_webscraper.py
+++ tests/test_webscraper.py
@@ -14,10 +14,19 @@ class WebScraperTest(unittest.TestCase):
         self.assertEqual(expected_games, get_all_games_for_date(2015, 10, 8))
 
     def test_get_game_summary_info(self):
-        pass
+        link = 'http://www.sports-reference.com/cfb/boxscores/2000-09-16-akron.html'
+        content = BeautifulSoup(requests.get(link).content, 'lxml')
+        expected_item = ('/cfb/boxscores/2000-09-16-akron.html',
+                         ('Central Florida', '/cfb/schools/central-florida/2000.html'), 24,
+                         ('Akron', '/cfb/schools/akron/2000.html'), 35)
+        self.assertIn(expected_item, get_game_summary_info(content))
 
     def test_get_game_team_stats(self):
-        pass
+        link = 'http://www.sports-reference.com/cfb/boxscores/2003-09-11-utah.html'
+        content = BeautifulSoup(requests.get(link).content, 'lxml')
+        expected_item = (('California', '/cfb/schools/california/2003.html'), 365, 66, 5.5, 19, 13,
+                         5, 1, 2, 10)
+        self.assertIn(expected_item, get_game_team_stats(content))
 
     def test_get_passing_stats(self):
         link = 'http://www.sports-reference.com/cfb/boxscores/2016-01-11-clemson.html'
@@ -52,7 +61,12 @@ class WebScraperTest(unittest.TestCase):
         self.assertIn(expected_item, get_return_stats(content))
 
     def test_get_kick_punt_stats(self):
-        pass
+        link = 'http://www.sports-reference.com/cfb/boxscores/2013-12-07-arizona-state.html'
+        content = BeautifulSoup(requests.get(link).content, 'lxml')
+        expected_item = (('Alex Garoutte', '/cfb/players/alex-garoutte-1.html'),
+                         ('Arizona State', '/cfb/schools/arizona-state/2013.html'),
+                         0, 0, 0, 0, 0, 0, 0, '4', '132', '33.0')
+        self.assertIn(expected_item, get_kick_punt_stats(content))
 
 
 if __name__ == '__main__':
