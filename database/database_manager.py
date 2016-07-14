653648678548809e81a702fdf0e6b91d605bec1f
Jacob Turpin <jacobeturpin@gmail.com>
2016-07-13 20:10:15 -0400

    change to load config settings via ConfigParser and began work on DatabaseManager class

--- database/database_manager.py
+++ database/database_manager.py
@@ -1,8 +1,21 @@
-import sqlalchemy
+import sqlalchemy as sa
 
 
 class DatabaseManager:
     """ Wrapper around sqlalchemy core to handle database interactions """
 
-    def __init__(self):
+    def __init__(self, config):
+        """ Instantiate an object of DatabaseManager """
+
+        self.dialect = config.get('dialect')
+        self.server = config.get('server')
+        self.database = config.get('database')
+        self.user = config.get('user')
+        self.__pwd = config.get('password')
+
+        conn_string = str.format('{}://{}:{}@{}/{}', self.dialect, self.user, self.__pwd,
+                                 self.server, self.database)
+        self.engine = sa.create_engine(conn_string)
+
+    def add_rows_to_table(self, table_name, vals):
         pass
--- runserver.py
+++ runserver.py
@@ -2,16 +2,22 @@
     for analysis
 """
 
+from configparser import ConfigParser
+
 from server import app
 
 
-def main():
+def run_debug_server():
     """ Test """
-    
+
+    config = ConfigParser()
+
+    config['DATABASE'].get('server')
+
     app.run('0.0.0.0', debug=True)
     return None
 
 
 if __name__ == '__main__':
     print('Testing main')
-    main()
+    run_debug_server()
